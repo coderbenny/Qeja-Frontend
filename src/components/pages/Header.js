71e56a31a265f5485bdc1d5ef90a9442b9ead020
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-04-29 02:43:01 +0300

    added icons to the navbar an

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { MdOutlineMapsHomeWork } from "react-icons/md";
+import { FaHouseUser, FaUserFriends, FaBuilding, FaComments, FaSignInAlt } from "react-icons/fa";
 
 function Header() {
   return (
-    <div className="fixed top-0 z-50 w-full bg-slate-900 text-white flex items-center justify-between p-1 h-16">
+    <div className="items-center fixed top-0 z-50 w-full bg-slate-900 text-white flex items-center justify-between p-1 h-16">
       <NavLink
         to="/"
         className="flex items-center ml-3 font-bold text-2xl tracking-wider cursor-pointer hover:text-blue-400"
@@ -12,24 +13,24 @@ function Header() {
         <MdOutlineMapsHomeWork className="" />
       </NavLink>
       <div className="flex justify-between">
-        <p className="mr-5 hover:text-blue-300 font-semibold cursor-pointer">
-          Rentals
+        <p className="flex items-center mr-5 hover:text-blue-300 font-semibold cursor-pointer">
+          <FaHouseUser className="mr-1" /> Rentals
         </p>
-        <p className="mr-5 hover:text-blue-300 font-semibold cursor-pointer">
-          Roommates
+        <p className="flex items-center mr-5 hover:text-blue-300 font-semibold cursor-pointer">
+          <FaUserFriends className="mr-1" /> Roommates
         </p>
-        <p className="mr-5 hover:text-blue-300 font-semibold cursor-pointer">
-          Property Owners
+        <p className="flex items-center mr-5 hover:text-blue-300 font-semibold cursor-pointer">
+          <FaBuilding className="mr-1" /> Property Owners
         </p>
-        <p className="mr-5 hover:text-blue-300 font-semibold cursor-pointer">
-          Community Chat
+        <p className="flex items-center mr-5 hover:text-blue-300 font-semibold cursor-pointer">
+          <FaComments className="mr-1" /> Community Chat
         </p>
       </div>
       <NavLink
-        className="mr-3 border-2 border-slate-400 text-white hover:bg-teal-400 hover:text-gray-800 font-semibold p-1 rounded-sm px-3 tracking-wider"
+        className="flex items-center mr-3 border-2 border-slate-400 text-white hover:bg-teal-400 hover:text-gray-800 font-semibold p-1 rounded-md px-3 tracking-wider"
         to="/login"
       >
-        Login
+        <FaSignInAlt className="mr-2" /> Login
       </NavLink>
       {/* <button className="p-1 px-3 bg-slate-700 rounded-md hover:bg-slate-800 border-2 border-gray-500 hover:border-gray-300" type="button">Register</button> */}
     </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import HomeSub from "../ui/HomeSub";
 import ReactTypingFxDemo from "../ui/ReactTypingEffectDemo";
+import FAQ from "./FAQ";
 
 import { IoIosArrowDropdown } from "react-icons/io";
 import { IoIosArrowDropup } from "react-icons/io";
@@ -13,45 +14,50 @@ function Home() {
   };
 
   return (
-    <div
-      className="flex flex-col items-center justify-center h-screen"
-      style={{
-        backgroundImage: `url(${process.env.PUBLIC_URL}/edited.jpg)`,
-        backgroundSize: "cover",
-        // backgroundAttachment: "fixed", // Make the background image fixed
-      }}
-    >
-      <div className="h-[180px]">
-        <h1 className="font-bold text-gradient-2 shadow-md tracking-widest text-9xl typing-effect uppercase bg-slate-900">
-          Qeja
-        </h1>
-        <ReactTypingFxDemo />
-      </div>
-      <div className="flex flex-col w-full items-center">
-        <div className="flex px-2 w-[550px] mb-4 justify-between h-[30px] items-center border-2 border-teal-500 rounded-xl text-gray-300 p-1 text-sm">
-          <p className="px-1 text-white" >
-            Register today and be part of a wonderful society
-          </p>
-          <span
-            className="flex items-center font-semibold text-slate-950 px-2 juatify-center font-semibold cursor-pointer bg-teal-400 text-sm rounded-md"
-            onClick={handleReadMoreClick}
-          >
-            {showHomeSub ? (
-                <>
-                    See Less <IoIosArrowDropup />
-                </>
-                ) : (
-                <>
-                    See More <IoIosArrowDropdown />
-                </>
-            )}
-          </span>
+    <>
+      <div
+        className="flex flex-col items-center justify-center h-screen"
+        style={{
+          backgroundImage: `url(${process.env.PUBLIC_URL}/edited.jpg)`,
+          backgroundSize: "cover",
+          // backgroundAttachment: "fixed", // Make the background image fixed
+        }}
+      >
+        <div className="h-[180px]">
+          <h1 className="font-bold text-gradient-2 shadow-md tracking-widest text-9xl typing-effect uppercase bg-slate-900">
+            Qeja
+          </h1>
+          <ReactTypingFxDemo />
         </div>
+        <div className="flex flex-col w-full items-center">
+          <div className="flex px-2 w-[550px] mb-4 justify-between h-[30px] items-center border-2 border-teal-500 rounded-xl text-gray-300 p-1 text-sm">
+            <p className="px-1 text-white" >
+              Register today and be part of a wonderful society
+            </p>
+            <span
+              className="flex items-center font-semibold text-slate-950 px-2 juatify-center font-semibold cursor-pointer bg-teal-400 text-sm rounded-md"
+              onClick={handleReadMoreClick}
+            >
+              {showHomeSub ? (
+                  <>
+                      See Less <IoIosArrowDropup />
+                  </>
+                  ) : (
+                  <>
+                      See More <IoIosArrowDropdown />
+                  </>
+              )}
+            </span>
+          </div>
 
-        {showHomeSub && <HomeSub />}
+          {showHomeSub && <HomeSub />}
 
+        </div>
       </div>
-    </div>
+      
+      <FAQ/>
+
+    </>
   );
 }
 

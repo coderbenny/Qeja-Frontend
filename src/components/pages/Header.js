d3c1b5605008018440658ed321523e398b0b6cd6
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-04-28 20:06:00 +0300

    intnstalled react icons, created the landing page, styled the headings, added navbar, installed typing effect

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-
+import { MdOutlineMapsHomeWork } from "react-icons/md";
 function Header(){
     return(
         <div className="flex justify-between p-1 h-[50px] w-full bg-slate-950 text-white items-center">
-            <h1 className="ml-3 font-bold text-2xl tracking-wider">Qeja</h1>
+            <h1 className="flex items-center ml-3 font-bold text-2xl tracking-wider text-teal-400 "><MdOutlineMapsHomeWork className="mr-1"/> Qeja</h1>
             <div className="flex justify-between">
-                <p className="mr-5 cursor-pointer">Rentals</p> 
-                <p className="mr-5 cursor-pointer">Roommates</p> 
-                <p className="mr-5 cursor-pointer">Property Owners</p> 
-                <p className="mr-5 cursor-pointer">Community Chat</p> 
+                <p className="mr-5 hover:text-blue-300 font-semibold cursor-pointer">Rentals</p> 
+                <p className="mr-5 hover:text-blue-300 font-semibold cursor-pointer">Roommates</p> 
+                <p className="mr-5 hover:text-blue-300 font-semibold cursor-pointer">Property Owners</p> 
+                <p className="mr-5 hover:text-blue-300 font-semibold cursor-pointer">Community Chat</p> 
             </div>
-            <button className="tracking-wider mr-3 bg-blue-700 text-white p-1 rounded-sm px-3" type="button">Login</button>
+            <button className="tracking-wider mr-3 border-2 border-slate-400 text-white hover:bg-teal-400 hover:text-gray-800 font-semibold p-1 rounded-sm px-3" type="button">Login</button>
             {/* <button className="p-1 px-3 bg-slate-700 rounded-md hover:bg-slate-800 border-2 border-gray-500 hover:border-gray-300" type="button">Register</button> */}
         </div>
     )
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import HomeSub from "../ui/HomeSub";
+import ReactTypingFxDemo from "../ui/ReactTypingEffectDemo";
 
-function Home(){
-    return(
-        <div className="">
+import { IoIosArrowDropdown } from "react-icons/io";
+import { IoIosArrowDropup } from "react-icons/io";
 
+function Home() {
+  const [showHomeSub, setShowHomeSub] = useState(false);
+
+  const handleReadMoreClick = () => {
+    setShowHomeSub(!showHomeSub);
+  };
+
+  return (
+    <div
+      className="flex flex-col items-center justify-center h-screen"
+      style={{
+        backgroundImage: `url(${process.env.PUBLIC_URL}/edited.jpg)`,
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed", // Make the background image fixed
+      }}
+    >
+      <div className="h-[180px]">
+        <h1 className="font-bold text-gradient-2 shadow-md tracking-widest text-9xl typing-effect uppercase bg-slate-900">
+          Qeja
+        </h1>
+        <ReactTypingFxDemo />
+      </div>
+      <div className="flex flex-col w-full items-center">
+        <div className="flex px-2 w-[550px] mb-4 justify-between h-[30px] items-center border-2 border-teal-500 rounded-xl text-gray-300 p-1 text-sm">
+          <p className="px-1">
+            Register today and be part of a wonderful society
+          </p>
+          <span
+            className="flex items-center font-semibold text-slate-950 px-2 juatify-center font-semibold cursor-pointer bg-teal-400 text-sm rounded-md"
+            onClick={handleReadMoreClick}
+          >
+            {showHomeSub ? (
+                <>
+                    See Less <IoIosArrowDropup />
+                </>
+                ) : (
+                <>
+                    See More <IoIosArrowDropdown />
+                </>
+            )}
+          </span>
         </div>
-    )
+
+        {showHomeSub && <HomeSub />}
+      </div>
+    </div>
+  );
 }
 
 export default Home;

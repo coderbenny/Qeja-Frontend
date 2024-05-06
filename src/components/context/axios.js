43f4f454bccf936fd0480b377db02be38044b913
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-05-06 19:10:09 +0300

    modified axios base url

--- src/components/context/axios.js
+++ src/components/context/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    
-    // Url for the backend
-    baseURL: "http://127.0.0.1:5555"
-})
+  // Url for the backend
+  baseURL: "https://qeja-backend.onrender.com",
+});

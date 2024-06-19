de244ac258e4ce5aa29d455152c57d3fe1256c09
benny <benny.mathew@student.moringaschool.com>
2024-06-19 18:54:00 +0300

    changed explore component bg

--- src/components/pages/Explore.js
+++ src/components/pages/Explore.js
@@ -14,10 +14,12 @@ function Explore() {
     const getPosts = async () => {
       try {
         setLoading(true);
-        const res = await axios.get(`/posts?page=${currentPage}&limit=${postsPerPage}`);
+        const res = await axios.get(
+          `/posts?page=${currentPage}&limit=${postsPerPage}`
+        );
         if (res.status === 200) {
           const data = res.data;
-          setPosts(prevPosts => [...prevPosts, ...data]);
+          setPosts((prevPosts) => [...prevPosts, ...data]);
         }
       } catch (error) {
         console.log("An error occurred:", error);
@@ -30,24 +32,35 @@ function Explore() {
   }, [currentPage]); // Trigger fetch when currentPage changes
 
   const handleLoadMore = () => {
-    setCurrentPage(prevPage => prevPage + 1);
+    setCurrentPage((prevPage) => prevPage + 1);
   };
 
   return (
-    <div className="p-10 bg-black text-white">
+    <div className="h-screen p-10">
       <div className="flex flex-col md:flex-row mt-20 h-auto">
         <Routes>
-          <Route path="/" element={<Feed posts={posts} loading={loading} onLoadMore={handleLoadMore} />} />
+          <Route
+            path="/"
+            element={
+              <Feed
+                posts={posts}
+                loading={loading}
+                onLoadMore={handleLoadMore}
+              />
+            }
+          />
         </Routes>
 
         {/* Sidebar */}
         <div className="text border-2 border-gray-600 p-3 w-full md:w-1/4 rounded-md hidden md:block">
-          <div className="flex flex-col bg-gray-400 rounded-md shadow-md justify-center items-center text-center h-[150px] mb-5">
-            <h1 className="font-bold text-4xl text-white">Some Information Here</h1>
-          </div>
+          <div className="flex flex-col bg-gray-200 w-[150px] rounded-full shadow-md justify-center mx-auto items-center text-center h-[150px] mb-5"></div>
           <div className="flex flex-col gap-2 justify-between">
-            <button type="button" className="p-2 bg-blue-500 text-white">Create Post</button>
-            <button type="button" className="p-2 bg-blue-500 text-white">Saved Posts</button>
+            <button type="button" className="p-2 bg-blue-500 text-white">
+              Create Post
+            </button>
+            <button type="button" className="p-2 bg-blue-500 text-white">
+              Saved Posts
+            </button>
           </div>
         </div>
       </div>
@@ -58,14 +71,19 @@ function Explore() {
 function Feed({ posts, loading, onLoadMore }) {
   return (
     <div className="flex flex-col w-full md:w-3/4 p-3 max-h-[630px] items-center">
-      <input className="font-bold mb-4 bg-transparent border-2 border-gray-500 text-white justify-center p-1 mx-auto rounded-md text-center w-[300px]" placeholder="Type here to search..." />
+      <input
+        className="font-bold mb-4 bg-transparent border-2 border-gray-500 text-white justify-center p-1 mx-auto rounded-md text-center w-[300px]"
+        placeholder="Type here to search..."
+      />
       <div className="max-h-[600px] overflow-y-auto">
         {posts && posts.length > 0
           ? posts.map((post, index) => <FeedPost key={index} post={post} />)
           : "No posts available"}
       </div>
       {loading && <div>Loading...</div>}
-      <button onClick={onLoadMore} className="mt-4 p-2 bg-blue-500 text-white">Load More</button>
+      <button onClick={onLoadMore} className="mt-4 p-2 bg-blue-500 text-white">
+        Load More
+      </button>
     </div>
   );
 }

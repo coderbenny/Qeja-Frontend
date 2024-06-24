9747cf018e0ef6efe245adc254a07a001db34a6d
benny <benny.mathew@student.moringaschool.com>
2024-06-24 11:50:34 +0300

    made the profile more responsive

--- src/components/pages/UserProfile.js
+++ src/components/pages/UserProfile.js
@@ -3,7 +3,6 @@ import { FaCamera } from "react-icons/fa";
 import useAuth from "../hooks/useAuth";
 import EditProfile from "../ui/EditProfile";
 import axios from "../context/axios";
-// import SwipeCard from "../ui/SwipeCard";
 import CustomTabPanel from "../ui/CustomTabPanel";
 
 function UserProfile() {
@@ -45,16 +44,12 @@ function UserProfile() {
     setEditing(true);
   };
 
-  const handleuploadpic = async () => {
-    console.log("Handling picture upload.");
-  };
-
   return (
     <div className="h-full p-5 bg-gradient-to-r from-blue-500 to-indigo-600">
       {!editing ? (
         <div className="flex flex-col items-center">
-          <div className="flex bg-white h-[300px] border-2 border-gray-300 rounded-md shadow-lg mb-4 w-full items-center justify-center p-4">
-            <div className="flex flex-col mr-3 mt-12 items-center justify-center">
+          <div className="flex flex-col md:flex-row bg-white h-auto md:h-[300px] border-2 border-gray-300 rounded-md shadow-lg mb-4 w-full items-center justify-center p-4">
+            <div className="flex flex-col mr-0 md:mr-3 mt-4 md:mt-12 items-center justify-center">
               <div className="relative mb-2 items-center flex mx-auto h-[150px] shadow-md border-2 border-gray-300 rounded-full w-[150px] justify-center overflow-hidden bg-gray-200">
                 <img
                   src={profile?.profile_pic || "/default-profile.png"}
@@ -71,7 +66,7 @@ function UserProfile() {
               </button>
             </div>
 
-            <div className="text-center items-center">
+            <div className="text-center items-center mt-4 md:mt-0">
               <div className="flex items-center w-full justify-between">
                 <h2 className="font-bold text-2xl text-gray-800 capitalize">
                   {user?.name || "Guest"}
@@ -87,12 +82,12 @@ function UserProfile() {
                   <p className="text-gray-600">{user?.following || "--"}</p>
                 </div>
               </div>
-              <div className="flex flex-col mb-2 w-[250px]">
+              <div className="flex flex-col mb-2 w-full md:w-[250px]">
                 <p className="text-left text-gray-700">{profile?.bio || ""}</p>
               </div>
             </div>
           </div>
-          <div className="bg-gray-200 h-[250px] rounded-md w-full shadow-md p-4">
+          <div className="bg-gray-200 h-auto md:h-[250px] rounded-md w-full shadow-md p-4">
             <CustomTabPanel />
           </div>
         </div>

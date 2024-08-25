9facf39da2bb07776221365286163ca083e666a1
benny <codebfullstack@gmail.com>
2024-08-25 10:52:03 +0300

    changed font color of FAQs

--- src/components/ui/DefaultAccordion.js
+++ src/components/ui/DefaultAccordion.js
@@ -1,26 +1,26 @@
-import * as React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-export default function AccordionExpandDefault({faq}) {
+export default function AccordionExpandDefault({ faq }) {
   return (
-    <div >
-      <Accordion >
+    <div>
+      <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1-content"
           id="panel1-header"
           className="bg-slate-950"
         >
-          <Typography ><span className="font-bold">{faq.title}</span></Typography>
+          <Typography>
+            <span className="font-bold text-blue-700">{faq.title}</span>
+          </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            {faq.content}
-          </Typography>
+          <Typography>{faq.content}</Typography>
         </AccordionDetails>
       </Accordion>
     </div>
